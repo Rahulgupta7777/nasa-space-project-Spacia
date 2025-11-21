38abcad4b083d2b1d92b3b34173d08ff5d20b018
Rahulgupta7777 <https://github.com/Rahulgupta7777>
2025-11-21 21:18:04 +0530

    updating home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import AnimatedHero from "@/components/AnimatedHero";
 import PillarsSection from "@/components/PillarsSection";
 import CTASection from "@/components/CTASection";
-import OrbitalGlobe from "@/components/OrbitalGlobe";
 import WorldviewShowcase from "@/components/WorldviewShowcase";
 
 export default function Home() {
